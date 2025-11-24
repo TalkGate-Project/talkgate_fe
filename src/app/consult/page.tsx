b9fdbf4a1b936e3e999842df35079f1ced3cecf1
GeonhaLee <113013790+ipjaworld@users.noreply.github.com>
2025-11-24 12:42:32 +0900

    modify: 채팅 영역 로직 수정 및 정상화 작업업

--- src/app/consult/page.tsx
+++ src/app/consult/page.tsx
@@ -19,8 +19,8 @@ export default function ConsultPage() {
   if (!projectId) return null;
 
   return (
-    <main className="min-h-[calc(100vh-54px)] bg-neutral-10">
-      <div className="mx-auto max-w-[1324px] w-full px-0 pt-9 pb-6">
+    <main className="h-[calc(100vh-54px)] bg-neutral-10 flex flex-col">
+      <div className="mx-auto max-w-[1324px] w-full px-0 pt-9 pb-6 flex-1 flex flex-col min-h-0">
         <ChatPanel projectId={projectId} />
       </div>
     </main>
