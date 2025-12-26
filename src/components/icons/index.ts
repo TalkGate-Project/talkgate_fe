d98cb64bea40983656523437eec8b5f2f7f82a8c
GeonhaLee <113013790+ipjaworld@users.noreply.github.com>
2025-12-26 18:12:11 +0900

    fix: 드로워의 아이콘 피그마와 일치시키며 다크모드에 대한 대응 추가

---
+++ src/components/icons/index.ts
@@ -0,0 +1,10 @@
+// 모바일 드로워 메뉴 아이콘들
+export { default as DashboardIcon } from './DashboardIcon';
+export { default as ConsultIcon } from './ConsultIcon';
+export { default as CustomerListIcon } from './CustomerListIcon';
+export { default as StatsIcon } from './StatsIcon';
+export { default as AttendanceIcon } from './AttendanceIcon';
+export { default as NoticeIcon } from './NoticeIcon';
+export { default as SettingsIcon } from './SettingsIcon';
+export { default as CloseIcon } from './CloseIcon';
+
